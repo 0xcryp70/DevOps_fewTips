c65c0956e20494f4f43ee39d002e452b16b643f2
0xcryp70 <saeed.lali@icloud.com>
2022-03-21 23:10:39 +0000

    update terraform

---
+++ Terraform/aws_demo/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+	default = "eu-central-1"
+}
+variable "AMIS" {
+	type = "map"
+	default = {
+		eu-central-1 = "ami-0498a49a15494604f"
+		us-east-1 = "ami-01896de1f162f0ab7"
+	}
+}
